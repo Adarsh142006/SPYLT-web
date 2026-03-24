3860e72f41013977b11679c39a53ba8d3fd115dd
Adarsh Singh <lucifer90054@gmail.com>
2026-03-24 14:07:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import HeroSection from './sections/HeroSection'
+import { ScrollTrigger } from 'gsap/all'
+import gsap from 'gsap'
+import MessageSection from './sections/MessageSection'
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+const App = () => {
+  return (
+    <main>
+      <Navbar/>
+      <HeroSection/>
+      <MessageSection/>
+      <div className='h-dvh border border-red-500'/>
+    </main>
+  )
+}
+
+export default App
